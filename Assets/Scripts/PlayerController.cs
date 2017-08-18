c72cf46e7ba793980646fab2f9b775ab1e552310
zenogears <zenogears.aka@gmail.com>
2017-08-18 13:44:13 +0300

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+	public LayerMask movementMask;
+
+	Camera cam;
+	PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		motor = GetComponent<PlayerMotor>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+
+				motor.MoveToPoint (hit.point);
+				// stop focusing any
+
+			}
+
+		}
+
+		if (Input.GetMouseButtonDown(1)) {
+
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100, movementMask)) {
+
+				// check if we hit an interactable
+				// if we did set it our focus
+
+			}
+
+		}
+	}
+}
